9bd8694ec14cabce43703ff8df872357b4fe1ec9
Joao Vitor <jvde.olivera@gmail.com>
2025-04-02 19:49:06 -0300

    Commit vai

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/si/review/.venv
+set -gx VIRTUAL_ENV /home/si/Buscas/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
